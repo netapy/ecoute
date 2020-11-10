d53b6047f004e8f339465484330c04a42a40660a
netapy <39877685+netapy@users.noreply.github.com>
2020-11-10 20:27:57 +0100

    V3 amazing lol

--- js/ect.js
+++ js/ect.js
@@ -1,5 +1,7 @@
+var p;
+
 function JeSuisLanceur() {
-    const p = new SimplePeer({
+    p = new SimplePeer({
         initiator: true,
         trickle: false
     })
@@ -9,6 +11,12 @@ function JeSuisLanceur() {
     p.on('signal', data => {
         console.log('SIGNAL', JSON.stringify(data))
         document.querySelector('#outgoing').textContent = JSON.stringify(data)
+
+        try {
+            p.send("NV_SDP_PR_VIDEO" + String(JSON.stringify(data)))
+        } catch (error) {
+            console.log(error, 'cest pas grave cest le premier contact.')
+        }
     })
 
     document.querySelector('form').addEventListener('submit', ev => {
@@ -22,33 +30,37 @@ function JeSuisLanceur() {
     })
 
     p.on('data', data => {
+        if (data.includes('NV_SDP_PR_VIDEO')) {
+            console.log("ON A RECU LE NOVEAU SDP DU CLIETN")
+            p.signal(JSON.parse(String(data).replace("NV_SDP_PR_VIDEO", "")))
+        }
         console.log('data: ' + data)
     })
 
-    //p.on('stream', stream => {
-    //    // got remote video stream, now let's show it in a video tag
-    //    var video = document.getElementById('video')
-    //
-    //    if ('srcObject' in video) {
-    //        video.srcObject = stream
-    //    } else {
-    //        video.src = window.URL.createObjectURL(stream) // for older browsers
-    //    }
-    //    video.play()
-    //})
+    p.on('stream', stream => {
+        // got remote video stream, now let's show it in a video tag
+        var video = document.querySelector('video')
+        if ('srcObject' in video) {
+            video.srcObject = stream
+        } else {
+            video.src = window.URL.createObjectURL(stream) // for older browsers
+        }
+        video.play()
+        console.log("la video doit jouer laa")
+    })
 
-    //function addMedia(stream) {
-    //    p.addStream(stream) // <- add streams to peer dynamically
-    //}
-    // then, anytime later...
-    //navigator.mediaDevices.getUserMedia({
-    //    video: true,
-    //    audio: true
-    //}).then(addMedia).catch(() => {})
+    function addMedia(stream) {
+        p.addStream(stream) // <- add streams to peer dynamically
+    }
+
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+    }).then(addMedia).catch(() => {})
 }
 
 function jeSuisRecepteur() {
-    const p = new SimplePeer({
+    p = new SimplePeer({
         initiator: false,
         trickle: false
     })
@@ -58,6 +70,12 @@ function jeSuisRecepteur() {
     p.on('signal', data => {
         console.log('SIGNAL', JSON.stringify(data))
         document.querySelector('#outgoing').textContent = JSON.stringify(data)
+
+        try {
+            p.send("NV_SDP_PR_VIDEO" + String(JSON.stringify(data)))
+        } catch (error) {
+            console.log(error, 'cest pas grave cest le premier contact.')
+        }
     })
 
     document.querySelector('form').addEventListener('submit', ev => {
@@ -71,29 +89,34 @@ function jeSuisRecepteur() {
     })
 
     p.on('data', data => {
+        if (data.includes('NV_SDP_PR_VIDEO')) {
+            console.log("ON A RECU LE NOVEAU SDP DU CLIETN")
+            p.signal(JSON.parse(String(data).replace("NV_SDP_PR_VIDEO", "")))
+        }
         console.log('data: ' + data)
     })
 
-    //p.on('stream', stream => {
-    //    // got remote video stream, now let's show it in a video tag
-    //    var video = document.getElementById('video')
-    //
-    //    if ('srcObject' in video) {
-    //        video.srcObject = stream
-    //    } else {
-    //        video.src = window.URL.createObjectURL(stream) // for older browsers
-    //    }
-    //    video.play()
-    //})
+    p.on('stream', stream => {
+        // got remote video stream, now let's show it in a video tag
+        var video = document.querySelector('video')
+        if ('srcObject' in video) {
+            video.srcObject = stream
+        } else {
+            video.src = window.URL.createObjectURL(stream) // for older browsers
+        }
+        video.play()
+        console.log("la video doit jouer laa")
+    })
 
-    //function addMedia(stream) {
-    //    p.addStream(stream) // <- add streams to peer dynamically
-    //}
-    // then, anytime later...
-    //navigator.mediaDevices.getUserMedia({
-    //    video: true,
-    //    audio: true
-    //}).then(addMedia).catch(() => {})
+    function addMedia(stream) {
+        p.addStream(stream) // <- add streams to peer dynamically
+    }
+
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+    }).then(addMedia).catch(() => {})
+
 }
 
 document.getElementById("zonePrincipalee").innerHTML = '<img src="assets/ecoute.svg" style="height: 150px; filter: brightness(1.1); position: absolute;">'
@@ -102,8 +125,8 @@ function changementDeMenu(bouton) {
     let dicoZones = {
         'EcranClasique': '<img src="assets/ecoute.svg" style="height: 150px; filter: brightness(1.1); position: absolute;">',
         'BtnParam': "<div style=\"padding: 10px;\"><h3>Ecoute.app</h3> <p><strong>Ecoute</strong> est entièrement libre d'utilisation et fonctionne entièrement sans utiliser vos données.<br>C'est comme utiliser un <strong>talkie-walkie</strong> sous stéroïdes.</p><p>-B</p></div>",
-        'BtnLaunchCom': '<form> Ton code :<br><textarea id="outgoing"></textarea><br>Son code :<br><textarea id="incoming"></textarea><br> <button type="submit">submit</button> </form><div>',
-        'BtnRecepCom': '<form> Son code :<br><textarea id="outgoing"></textarea><br> <button type="submit">submit</button> <br>Son code :<br><textarea id="incoming"></textarea></form><div>',
+        'BtnLaunchCom': '<form><h2>Init</h2><video width="320" height="240" controls></video> Donne lui ton code :<br><textarea id="outgoing"></textarea><br>Rentre son code : :<br><textarea id="incoming"></textarea><br> <button type="submit">Valider</button> </form><div>',
+        'BtnRecepCom': '<form><h2>Recep</h2><video width="320" height="240" controls></video><br><br> Rentre son code :<br><textarea id="incoming"></textarea><br> <button type="submit">Valider</button> <br>Donne lui ton code :<br><textarea id="outgoing"></textarea></form><div>',
     }
     zoneParamID = document.getElementById("zonePrincipalee")
     if (String(dicoZones[bouton.id]).trim() != String(zoneParamID.innerHTML).trim()) {
@@ -111,11 +134,18 @@ function changementDeMenu(bouton) {
         setTimeout(() => {
             zonePrincipalee.innerHTML = dicoZones[bouton.id];
             zoneParamID.style.transform = "rotateX(0deg)";
-
             if (bouton.id == "BtnLaunchCom") {
                 JeSuisLanceur()
+            } else if (bouton.id == "BtnRecepCom") {
+                jeSuisRecepteur()
             }
-
         }, 300);
+    } else {
+        zoneParamID.style.transform = "rotateX(-90deg)";
+        setTimeout(() => {
+            zonePrincipalee.innerHTML = dicoZones["EcranClasique"];
+            zoneParamID.style.transform = "rotateX(0deg)";
+        }, 300);
+
     }
 }
