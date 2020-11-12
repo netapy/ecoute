016e4b819b2e69351c0e452587f43ecea3ff642f
netapy <39877685+netapy@users.noreply.github.com>
2020-11-12 13:06:02 +0100

    Avec PeerJS et signalling chez la mif, marche bien !!

--- js/ect.js
+++ js/ect.js
@@ -1,122 +1,39 @@
-var p;
+var peer;
+var conn;
 
 function JeSuisLanceur() {
-    p = new SimplePeer({
-        initiator: true,
-        trickle: false
-    })
+    console.log("démarrage co")
+    let iddMoi = document.getElementById('SaisieIDd').value;
 
-    p.on('error', err => console.log('error', err))
+    peer = new Peer(iddMoi, {
+        host: '176.159.35.44',
+        port: 9000,
+        path: '/ecouteapp'
+    });
+    peer.on('open', function (id) {
+        console.log('My peer ID is: ' + String(id));
+    });
 
-    p.on('signal', data => {
-        console.log('SIGNAL', JSON.stringify(data))
-        document.querySelector('#outgoing').textContent = JSON.stringify(data)
-
-        try {
-            p.send("NV_SDP_PR_VIDEO" + String(JSON.stringify(data)))
-        } catch (error) {
-            console.log(error, 'cest pas grave cest le premier contact.')
-        }
-    })
-
-    document.querySelector('form').addEventListener('submit', ev => {
-        ev.preventDefault()
-        p.signal(JSON.parse(document.querySelector('#incoming').value))
-    })
-
-    p.on('connect', () => {
-        console.log('CONNECT')
-        p.send('whateveeeeer' + Math.random())
-    })
-
-    p.on('data', data => {
-        if (data.includes('NV_SDP_PR_VIDEO')) {
-            console.log("ON A RECU LE NOVEAU SDP DU CLIETN")
-            p.signal(JSON.parse(String(data).replace("NV_SDP_PR_VIDEO", "")))
-        }
-        console.log('data: ' + data)
-    })
-
-    p.on('stream', stream => {
-        // got remote video stream, now let's show it in a video tag
-        var video = document.querySelector('video')
-        if ('srcObject' in video) {
-            video.srcObject = stream
-        } else {
-            video.src = window.URL.createObjectURL(stream) // for older browsers
-        }
-        video.play()
-        console.log("la video doit jouer laa")
-    })
-
-    function addMedia(stream) {
-        p.addStream(stream) // <- add streams to peer dynamically
-    }
-
-    //navigator.mediaDevices.getUserMedia({
-    //    video: true,
-    //    audio: false
-    //}).then(addMedia).catch(() => {})
+    peer.on('connection', function (cc) {
+        console.log("connexion etabllie !!!! ");
+        conn = cc;
+        conn.on('data', function (data) {
+            alert(data);
+        });
+    });
 }
 
-function jeSuisRecepteur() {
-    p = new SimplePeer({
-        initiator: false,
-        trickle: false
-    })
+function Connexion() {
+    let iddContact = document.getElementById('IdDuContact').value;
+    conn = peer.connect(iddContact)
+    conn.on('data', function (data) {
+        alert(data);
+    });
+}
 
-    p.on('error', err => console.log('error', err))
-
-    p.on('signal', data => {
-        console.log('SIGNAL', JSON.stringify(data))
-        document.querySelector('#outgoing').textContent = JSON.stringify(data)
-
-        try {
-            p.send("NV_SDP_PR_VIDEO" + String(JSON.stringify(data)))
-        } catch (error) {
-            console.log(error, 'cest pas grave cest le premier contact.')
-        }
-    })
-
-    document.querySelector('form').addEventListener('submit', ev => {
-        ev.preventDefault()
-        p.signal(JSON.parse(document.querySelector('#incoming').value))
-    })
-
-    p.on('connect', () => {
-        console.log('CONNECT')
-        p.send('whateveeeeer' + Math.random())
-    })
-
-    p.on('data', data => {
-        if (data.includes('NV_SDP_PR_VIDEO')) {
-            console.log("ON A RECU LE NOVEAU SDP DU CLIETN")
-            p.signal(JSON.parse(String(data).replace("NV_SDP_PR_VIDEO", "")))
-        }
-        console.log('data: ' + data)
-    })
-
-    p.on('stream', stream => {
-        // got remote video stream, now let's show it in a video tag
-        var video = document.querySelector('video')
-        if ('srcObject' in video) {
-            video.srcObject = stream
-        } else {
-            video.src = window.URL.createObjectURL(stream) // for older browsers
-        }
-        video.play()
-        console.log("la video doit jouer laa")
-    })
-
-    function addMedia(stream) {
-        p.addStream(stream) // <- add streams to peer dynamically
-    }
-
-    //navigator.mediaDevices.getUserMedia({
-    //    video: true,
-    //    audio: false
-    //}).then(addMedia).catch(() => {})
-
+function SendMessage() {
+    let msgAEnvoyer = document.getElementById('idmsgAEnvoyer').value;
+    conn.send(msgAEnvoyer)
 }
 
 document.getElementById("zonePrincipalee").innerHTML = '<img src="assets/ecoute.svg" style="height: 150px; filter: brightness(1.1); position: absolute;">'
@@ -124,9 +41,12 @@ document.getElementById("zonePrincipalee").innerHTML = '<img src="assets/ecoute.
 function changementDeMenu(bouton) {
     let dicoZones = {
         'EcranClasique': '<img src="assets/ecoute.svg" style="height: 150px; filter: brightness(1.1); position: absolute;">',
-        'BtnParam': "<div style=\"padding: 10px;\"><h3>Ecoute.app</h3> <p><strong>Ecoute</strong> est entièrement libre d'utilisation et fonctionne entièrement sans utiliser vos données.<br>C'est comme utiliser un <strong>talkie-walkie</strong> sous stéroïdes.</p><p>-B</p></div>",
-        'BtnLaunchCom': '<form><h2>Init</h2><video width="320" height="240" controls></video> Donne lui ton code :<br><textarea id="outgoing"></textarea><br>Rentre son code : :<br><textarea id="incoming"></textarea><br> <button type="submit">Valider</button> </form><div>',
-        'BtnRecepCom': '<form><h2>Recep</h2><video width="320" height="240" controls></video><br><br> Rentre son code :<br><textarea id="incoming"></textarea><br> <button type="submit">Valider</button> <br>Donne lui ton code :<br><textarea id="outgoing"></textarea></form><div>',
+
+        'BtnParam': "<div style=\"padding: 10px;\"><h3>Ecoute.app</h3><p><strong>Ecoute</strong> est entièrement libre d'utilisation et fonctionne entièrement sans utiliser vos données.<br>C'est comme utiliser un <strong>talkie-walkie</strong> sous stéroïdes.</p><p>-B</p></div>",
+
+        'BtnLaunchCom': '<h2>Init</h2><input type="text" placeholder="Choisis ton ID" id="SaisieIDd"><button onclick="JeSuisLanceur()">Definition</button><br><input type="text" placeholder="ID de ton contact (laisse vide si tes le 1)" id="IdDuContact"><button onclick="Connexion()">Connexion</button><br>',
+
+        'BtnRecepCom': '<h2>Messages</h2><input type="text" placeholder="Message..." id="idmsgAEnvoyer"><button onclick="SendMessage()">Send</button>',
     }
     zoneParamID = document.getElementById("zonePrincipalee")
     if (String(dicoZones[bouton.id]).trim() != String(zoneParamID.innerHTML).trim()) {
@@ -134,11 +54,6 @@ function changementDeMenu(bouton) {
         setTimeout(() => {
             zonePrincipalee.innerHTML = dicoZones[bouton.id];
             zoneParamID.style.transform = "rotateX(0deg)";
-            if (bouton.id == "BtnLaunchCom") {
-                JeSuisLanceur()
-            } else if (bouton.id == "BtnRecepCom") {
-                jeSuisRecepteur()
-            }
         }, 300);
     } else {
         zoneParamID.style.transform = "rotateX(-90deg)";
