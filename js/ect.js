ba27c7991ad39a24d5bc155b391f1dedc3c25f45
netapy <39877685+netapy@users.noreply.github.com>
2020-11-14 19:45:46 +0100

    version vraiment cool

--- js/ect.js
+++ js/ect.js
@@ -2,77 +2,105 @@ var peer;
 var conn;
 var call;
 var idAutre;
-var idMoi;
+var idMoi = "";
+let SignalingHost = {
+    'host': "www.ecoute.app",
+    'port': 9000,
+    'path': "/ecouteapp"
+};
+let fakeBtnMenu = [{
+    "id": "returnArrow"
+}, {
+    "id": "BtnUIMessages"
+}, {
+    "id": "BtnConnaissance"
+}];
+let idDefini = false;
 
-function JeSuisLanceur() {
-    console.log("démarrage co")
-    let iddMoi = document.getElementById('SaisieIDd').value;
 
-    peer = new Peer(iddMoi, {
-        host: 'www.ecoute.app',
-        port: 9000,
-        path: '/ecouteapp'
+function JeSuisLanceur(mode) {
+    //let iddMoi = document.getElementById('SaisieIDd').value; ct pour choisir son id lol
+    if (idDefini == false) peer = new Peer(idMoi, {
+        host: SignalingHost["host"],
+        port: SignalingHost["port"],
+        path: SignalingHost["path"]
     });
 
     peer.on('open', function (id) {
         console.log('My peer ID is: ' + String(id));
-        idMoi = String(id)
+        idMoi = String(id);
+        document.getElementById("monIdFrr").innerHTML = idMoi;
+        new QRCode(document.getElementById("qrcode"), {
+            text: idMoi,
+            width: 128,
+            height: 128,
+            colorLight: "#eeeeee"
+        });
+        idDefini = true;
     });
+    if (idMoi != "") {
+        document.getElementById("monIdFrr").innerHTML = idMoi;
+    }
 
     peer.on('error', err => {
         console.log(err)
     });
 
     peer.on('connection', function (cc) {
-        console.log("connexion etabllie !!!! ");
-        document.getElementById("txtStatut").innerHTML = "Connecté"
+        console.log("connexion entrante ");
+
         conn = cc;
         idAutre = conn.peer;
         conn.on('data', function (data) {
             alert(data);
         });
+        conn.on('close', function (data) {
+            alert("L'un de vous s'est deconnecté.")
+            changementDeMenu(fakeBtnMenu[0])
+        });
+        changementDeMenu(fakeBtnMenu[1])
     });
 
     peer.on('disconnected', function () {
-        console.log("reconnexion...")
+        console.log("reconnexion...");
         peer.reconnect();
     });
 
     peer.on('call', function (appel_entrant) {
-        console.log("appel entrant!")
+        console.log("appel entrant!");
         call = appel_entrant;
         call.on('stream', function (streamOfPeer) {
             videoDiv = document.getElementById("vidFeedback");
+            videoDiv.style.display = "";
             videoDiv.srcObject = streamOfPeer;
             videoDiv.play();
         });
-
-        navigator.mediaDevices.getUserMedia({
-                video: true,
-                audio: false
-            }).then(function (stream) {
-                call.answer(stream);
-            })
-            .catch(function (err) {
-                console.log(err)
-                alert("Faudra activer ta cam pour répondre à l'appel.")
-            });
+        call.answer();
     });
+    localStorage.removeItem('codeAmi');
 }
 
 function Connexion() {
-    let iddContact = document.getElementById('IdDuContact').value;
-    idAutre = iddContact;
-    conn = peer.connect(iddContact);
-    document.getElementById("txtStatut").innerHTML = "Connecté";
-    conn.on('data', function (data) {
-        alert(data);
-    });
+    if (document.querySelector("#idDuContact").value != "") {
+        let iddContact = document.getElementById('IdDuContact').value;
+        conn = peer.connect(iddContact);
+        idAutre = iddContact;
+        conn.on('data', function (data) {
+            alert(data);
+        });
+        conn.on('close', function (data) {
+            alert("L'un de vous s'est deconnecté.")
+            changementDeMenu(fakeBtnMenu[0])
+        });
+        changementDeMenu(fakeBtnMenu[1])
+    } else {
+        alert("Rentre le nom de ton contact.")
+    }
 }
 
 function SendMessage() {
     let msgAEnvoyer = document.getElementById('idmsgAEnvoyer').value;
-    conn.send(msgAEnvoyer)
+    conn.send(msgAEnvoyer);
 }
 
 function CallDude() {
@@ -80,40 +108,77 @@ function CallDude() {
             video: true,
             audio: false
         }).then(function (stream) {
-            console.log("lancement appel... a " + String(idAutre))
+            console.log("lancement appel... a " + String(idAutre));
             call = peer.call(idAutre, stream);
         })
         .catch(function (err) {
-            console.log(err)
-            alert("Faudra activer ta cam pour lancer l'appel.")
+            console.log(err);
+            alert("Faudra activer ta cam pour lancer l'appel.");
         });
 }
 
-document.getElementById("zonePrincipalee").innerHTML = '<img src="assets/ecoute.svg" style="height: 150px; filter: brightness(1.1); position: absolute;">'
+var dicoZones = {
+    'returnArrow': '<img src="assets/ecoute.svg" style="height: 150px; filter: brightness(1.1);"><input class="inputEcoute" id="inputChanmax" placeholder="Ton nom...">',
+
+    'BtnAleatoire': '<img src="assets/ecoute.svg" style="height: 100px; filter: brightness(1.1); opacity:.5">Mode aléatoire en construction.',
+
+    'BtnParam': "<div style='padding: 10px; max-width:550px;'><h4>Ecoute.app</h4><p><strong>Ecoute</strong> est entièrement libre d'utilisation et fonctionne entièrement sans utiliser tes données.<br>C'est comme utiliser un <strong>talkie-walkie</strong> sous stéroïdes. Utilise le sans modération. Tout ce qui se passe ici reste entre toi et ton interlocuteur. </p><p>-B</p></div>",
+
+    'BtnConnaissance': '<h4>Mon identifiant:</h4><span id="monIdFrr"></span><div id="qrcode"></div><hr><span><input class="inputEcoute" type="text" placeholder="Nom de ton ami" id="IdDuContact"><button onClick="alert(\'Camera QR code !\')">QR</button></span><button id="btn-connex" onclick="Connexion()">Connexion</button><br>',
+
+    'BtnUIMessages': '<div><h2 id="titreConv">Messages</h2><video id="vidFeedback" width="100%"></video><button onclick="CallDude()">Camera</button><span><div class="smsContainer"></div><input type="text" class="inputEcoute" placeholder="Message..." id="idmsgAEnvoyer"><button onclick="SendMessage()">Send</button></span></div>',
+}
+
+document.getElementById("zonePrincipalee").innerHTML = dicoZones["returnArrow"];
+document.querySelector("#inputChanmax").value = localStorage.getItem("pseudoAvant");
 
 function changementDeMenu(bouton) {
-    let dicoZones = {
-        'EcranClasique': '<img src="assets/ecoute.svg" style="height: 150px; filter: brightness(1.1); position: absolute;">',
-
-        'BtnParam': "<div style=\"padding: 10px;\"><h3>Ecoute.app</h3><p><strong>Ecoute</strong> est entièrement libre d'utilisation et fonctionne entièrement sans utiliser vos données.<br>C'est comme utiliser un <strong>talkie-walkie</strong> sous stéroïdes.</p><p>-B</p></div>",
-
-        'BtnLaunchCom': '<h2 id="txtStatut">Init</h2><input type="text" placeholder="Choisis ton ID" id="SaisieIDd"><button onclick="JeSuisLanceur()">Definition</button><br><input type="text" placeholder="ID de ton contact (laisse vide si tes le 1)" id="IdDuContact"><button onclick="Connexion()">Connexion</button><br>',
-
-        'BtnRecepCom': '<h2>Messages</h2><video id="vidFeedback" width="320" height="240" controls></video><button onclick="CallDude()">Call</button><input type="text" placeholder="Message..." id="idmsgAEnvoyer"><button onclick="SendMessage()">Send</button>',
+    let zoneParamID = document.getElementById("zonePrincipalee");
+    let zoneBoutons = document.querySelector("#boutonsMenu");
+    let flecheRetour = document.querySelector("#returnArrow");
+    if (idMoi == "" && document.querySelector("#inputChanmax").value != "") {
+        idMoi = String(document.querySelector("#inputChanmax").value) + '-' + String(Math.floor(Math.random() * 100));
+        localStorage.setItem("pseudoAvant", document.querySelector("#inputChanmax").value)
     }
-    zoneParamID = document.getElementById("zonePrincipalee")
     if (String(dicoZones[bouton.id]).trim() != String(zoneParamID.innerHTML).trim()) {
         zoneParamID.style.transform = "rotateX(-90deg)";
-        setTimeout(() => {
+        if (bouton.id == "BtnUIMessages") {
+            zoneBoutons.style.transform = "translateY(50vh)";
+        } else {
+            zoneParamID.style.height = "60vh";
+            zoneBoutons.style.display = "";
+        }
+        setTimeout(() => { //to wait for animation heheh
             zonePrincipalee.innerHTML = dicoZones[bouton.id];
             zoneParamID.style.transform = "rotateX(0deg)";
-        }, 300);
+            if (bouton.id == "BtnUIMessages") {
+                zoneParamID.style.height = "85%";
+                zoneBoutons.style.display = "none";
+                flecheRetour.style.transform = "translateX(0)";
+            } else {
+                flecheRetour.style.transform = "translateX(50px)";
+                zoneBoutons.style.transform = "translateY(0vh)"
+            }
+            if (bouton.id == "BtnConnaissance") {
+                if (localStorage.getItem("codeAmi") != null) document.querySelector("#IdDuContact").value = idAutre
+                JeSuisLanceur(bouton.id)
+            }
+        }, 400);
     } else {
         zoneParamID.style.transform = "rotateX(-90deg)";
         setTimeout(() => {
-            zonePrincipalee.innerHTML = dicoZones["EcranClasique"];
+            zonePrincipalee.innerHTML = dicoZones["returnArrow"];
             zoneParamID.style.transform = "rotateX(0deg)";
-        }, 300);
-
+        }, 400);
     }
-}
+    setTimeout(() => {
+        if (document.querySelector("#inputChanmax") != null) document.querySelector("#inputChanmax").value = localStorage.getItem("pseudoAvant");
+    }, 500)
+};
+
+
+if (localStorage.getItem("codeAmi") != null) {
+    console.log("code ami! ")
+    idAutre = localStorage.getItem("codeAmi");
+    changementDeMenu(fakeBtnMenu[2]);
+};
