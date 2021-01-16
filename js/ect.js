90c95d45b322ce442ce89ed37873df4214b5e779
netapy <39877685+netapy@users.noreply.github.com>
2021-01-16 12:28:35 +0100

    Nouvelle interface !

--- js/ect.js
+++ js/ect.js
@@ -1,4 +1,5 @@
 var lastPressed, peer, conn, call, idAutre;
+var interlocuteurs = [];
 "serviceWorker" in navigator && navigator.serviceWorker.register("service-worker.js");
 var idMoi = "";
 let SignalingHost = {
@@ -56,32 +57,39 @@ function JeSuisLanceur(e) {
     })), peer.on("disconnected", (function () {
         peer.reconnect()
     })), peer.on("call", (function (e) {
-        call = e, paramCall(), call.answer(streamLocal)
-    })), localStorage.removeItem("codeAmi")
+        call = e;
+        paramCall();
+        call.answer();
+    }))
+    localStorage.removeItem("codeAmi");
 }
 
 function paramCall() {
     call.on("stream", (function (e) {
-        affCachTxtDivs("masquer");
-        let t = document.getElementById("vidYou");
-        t.srcObject = e, t.play(), document.getElementsByClassName("vidCont")[0].style.display = "block"
+        console.log(call.peer);
+        newVidChat(e, call.peer);
     })), call.on("close", (function () {
-        affCachTxtDivs("afficher")
+        console.log('call ferme');
     }))
 };
 
 function clsCall() {
-    streamLocal.getTracks().forEach(e => e.stop()), document.getElementsByClassName("vidCont")[1].style.display = "none", affCachTxtDivs("afficher")
+    streamLocal.getTracks().forEach(e => e.stop());
+    document.getElementsByClassName("vidCont")[1].style.display = "none";
 };
 
 function Connexion() {
     let e = document.getElementById("IdDuContact").value;
-    conn = peer.connect(e), idAutre = e, paramConn()
+    conn = peer.connect(e);
+    idAutre = e;
+    paramConn();
 };
 
 function paramConn() {
     conn.on("data", (function (e) {
-        divSms = document.querySelector("#smsContainer"), divSms.insertAdjacentHTML("beforeend", "<div style='text-align: left;' class='smsTxt'>" + String(e) + "</div>"), divSms.scrollTop = divSms.scrollHeight
+        divSms = document.querySelector("#smsContainer");
+        divSms.insertAdjacentHTML("beforeend", "<div style='text-align: left;' class='smsTxt'>" + String(e) + "</div>");
+        divSms.scrollTop = divSms.scrollHeight;
     })), conn.on("close", (function (e) {
         changementDeMenu(fakeBtnMenu[0]);
         try {
@@ -97,21 +105,10 @@ function SendMessage() {
     t.insertAdjacentHTML("beforeend", "<div class='smsTxt' style='text-align: right; opacity:.7;'>" + String(e.value) + "</div>"), e.value = "", t.scrollTop = t.scrollHeight
 }
 
-function catchVideoStream(e) {
-    affCachTxtDivs("masquer");
-    streamLocal = e;
-    let t = document.getElementById("vidMe");
-    t.srcObject = e;
-    t.play();
-    document.getElementsByClassName("vidCont")[1].style.display = "block";
-    call = peer.call(idAutre, streamLocal);
-    paramCall();
-}
-
 function CallDude(e) {
-    try {
-        streamLocal.getTracks().forEach(e => e.stop())
-    } catch (e) {}
+    // try {
+    //     streamLocal.getTracks().forEach(e => e.stop())
+    // } catch (e) {}
     let videoStreamm;
     navigator.mediaDevices.getUserMedia({
         audio: true
@@ -132,8 +129,10 @@ function CallDude(e) {
             });
             [videoStreamm] = videoStream.getVideoTracks();
             theStream.addTrack(videoStreamm);
-        }
-        catchVideoStream(theStream);
+        };
+        newVidChat(theStream, "it-sm-ee")
+        call = peer.call(idAutre, streamLocal);
+        paramCall();
     }).catch(e => {
         throw e
     });
@@ -144,20 +143,32 @@ var dicoZones = {
     BtnAleatoire: '<img src="assets/ecoute.svg" style="height: 100px; filter: brightness(1.1); opacity:.5">Mode productif en construction.',
     BtnParam: "<div style='padding: 10px; max-width:550px;'><h5>Ecoute,</h5><p>Dès l'instant où la connexion est établie entre vous, plus rien n'existe en dehors de votre conversation. <br>Pas de serveurs, publicités, trackers... Rien.<br>Lorsque tout disparaît, il ne reste plus que vous, votre parole et votre <strong>écoute.</strong></p><p>Profitez, personne ne vous regarde.</p><p>-B</p></div>",
     BtnConnaissance: '<h4>Toi :</h4><div id="monIdFrr" onclick="copyToClipboard();swal(\'Ton lien a bien été copié.\')"></div><div id="qrcode"></div><hr><h4>Lui/Elle :</h4><span style="width:60%"><input class="inputEcoute col" type="text" placeholder="Son nom unique..." id="IdDuContact"></span><button id="btn-connex" onclick="Connexion()" disabled>Connexion</button>',
-    BtnUIMessages: '<div style="display: flex; flex-flow: column; height: 100%; width:95%;"><h4 id="titreConv">_messages</h4><div class="convVidContainer"></div><div class="row text-center"><div class="col-md-4 col-s-12"><button id="callBtn" class="buttonEct" onclick="CallDude(\'video\')">📷 Appel vidéo</button></div><div class="col-md-4 col-s-12 d-none d-md-block"><button id="callBtn" class="buttonEct" onclick="CallDude(\'ecran\')">💻 Partage d\'écran</button></div><div class="col-md-4 col-s-12"><button id="callBtn" class="buttonEct" onclick="newVidChat()">📞 Appel audio</button></div></div><div class="txtDiv" id="smsContainer"></div><span class="txtDiv"><input type="text" class="col-10 inputEcoute" style="background-color: #efefefbe;" placeholder="Message..." id="idmsgAEnvoyer"><button class="col-2 buttonEct" onclick="SendMessage();" style="background-color: transparent;"><img src="assets/send.svg"></button></span></div>'
+    BtnUIMessages: '<div style="display: flex; flex-flow: column; height: 100%; width:95%;"><h4 id="titreConv">_messages</h4><div class="convVidContainer"></div><div class="row text-center"><div class="col-md-4 col-s-12"><button id="callBtn" class="buttonEct" onclick="CallDude(\'video\')">📷 Appel vidéo</button></div><div class="col-md-4 col-s-12 d-none d-md-block"><button id="callBtn" class="buttonEct" onclick="CallDude(\'ecran\')">💻 Partage d\'écran</button></div><div class="col-md-4 col-s-12"><button id="callBtn" class="buttonEct" onclick="CallDude(\'audio\')">📞 Appel vocal</button></div></div><div class="txtDiv" id="smsContainer"></div><span class="txtDiv"><input type="text" class="col-10 inputEcoute" style="background-color: #efefefbe;" placeholder="Message..." id="idmsgAEnvoyer"><button class="col-2 buttonEct" onclick="SendMessage();" style="background-color: transparent;"><img src="assets/send.svg"></button></span></div>'
 };
 
-const newVidChat = () => {
-    document.querySelector(".convVidContainer").insertAdjacentHTML("afterbegin", "<div class='vidbloc'><img id='min' onclick='vidFullScreen(this)' src='assets/0.jfif'/></div>")
-}
+const newVidChat = (viddt, identif) => {
+    if (!document.body.contains(document.querySelector('#a' + identif))) {
+        streamLocal = viddt;
+        let box = document.createElement('div');
+        box.classList.add("vidbloc");
+        let vid = box.insertAdjacentElement("afterbegin", document.createElement('video'));
+        vid.setAttribute('data-etatcarre', 'min');
+        vid.id = "a" + identif;
+        vid.setAttribute('onclick', 'vidFullScreen(this)');
+        vid.srcObject = viddt;
+        vid.play();
+        document.querySelector(".convVidContainer").insertAdjacentElement("afterbegin", box);
+        interlocuteurs.push(idAutre)
+    }
+};
 
 const vidFullScreen = (el) => {
-    if (el.id == "min") {
+    if (el.dataset.etatcarre == "min") {
         el.style = "position: absolute; height: 100vw; width: auto; z-index:999; box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);  top: 50%; left: 50%; transform: translate(-50%, -50%);";
-        el.id = "hii";
+        el.dataset.etatcarre = "hii";
     } else {
         el.style = '';
-        el.id = 'min';
+        el.dataset.etatcarre = "min";
     }
 }
 
@@ -195,9 +206,6 @@ function copyToClipboard() {
 document.getElementById("zonePrincipalee").innerHTML = dicoZones.returnArrow, document.querySelector("#inputChanmax").value = localStorage.getItem("pseudoAvant"), null != localStorage.getItem("codeAmi") && (idAutre = localStorage.getItem("codeAmi"), changementDeMenu(fakeBtnMenu[2]), document.querySelector("#boutonsMenu").style.transform = "translateY(50vh)");
 var borderStyleSheet = document.createElement("style");
 
-function affCachTxtDivs(e) {
-    borderStyleSheet.disabled = "masquer" != e
-}
 
 function closeBackToMenu(e) {
     swal("T'es sûr de vouloir quitter la conv ?", {
@@ -219,6 +227,5 @@ function closeBackToMenu(e) {
         }
     })
 }
-document.head.appendChild(borderStyleSheet), borderStyleSheet.sheet.insertRule("@media (min-aspect-ratio: 2/5) and (max-width: 767px) {.txtDiv{display: none}}", 0), affCachTxtDivs("afficher");
 
-changementDeMenu(fakeBtnMenu[1])
+//changementDeMenu(fakeBtnMenu[1])
