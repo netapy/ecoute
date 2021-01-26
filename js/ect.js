9c79638119ddc3426c70f674666b9d5d04e4e132
netapy <39877685+netapy@users.noreply.github.com>
2021-01-26 20:42:52 +0100

    bonne version

--- js/ect.js
+++ js/ect.js
@@ -22,7 +22,7 @@ let SignalingHost = {
     idDefini = !1,
     paramVid = {
         frameRate: {
-            ideal: 16,
+            ideal: 15,
             max: 16
         },
         width: 480,
@@ -31,19 +31,25 @@ let SignalingHost = {
 var streamLocal, twocall, displHelp = !1;
 
 function JeSuisLanceur(e) {
-    peer = new Peer(idMoi, {
-        host: SignalingHost.host,
-        port: SignalingHost.port,
-        path: SignalingHost.path
-    });
     let qrGen = () => new QRCode(document.getElementById("qrcode"), {
         text: "https://ecoute.app/" + String(idMoi),
         width: 80,
         height: 80,
         colorLight: "#eeeeee"
     });
+
+    if (!idDefini) {
+        peer = new Peer(idMoi, {
+            host: SignalingHost.host,
+            port: SignalingHost.port,
+            path: SignalingHost.path
+        });
+    } else {
+        document.getElementById("monIdFrr").innerHTML = '<span class="shyUrl">ecoute.app/</span>' + idMoi;
+        qrGen();
+    }
+    
     peer.on("open", (function (e) {
-        console.log("peer on open !");
         idMoi = String(e);
         uiConnex("connecte");
         idDefini = !0;
@@ -52,8 +58,6 @@ function JeSuisLanceur(e) {
     }));
 
     peer.on("error", e => {
-        console.log("peer on error !");
-        console.log(e)
         "peer-unavailable" == e.type && swal("Désolé", "L'utilisateur n'existe pas ou n'est pas connecté.", "error").then(e => {
             changementDeMenu(fakeBtnMenu[0]);
             try {
@@ -72,16 +76,15 @@ function JeSuisLanceur(e) {
             .then((value) => {
                 switch (value) {
                     case "accept":
-                        console.log("peer on connection !");
                         idAutre = e.peer;
 
                         listeConnexions[indexConn] = e;
                         listePaires.push(e.peer);
                         paramConn(listeConnexions[indexConn]);
-                        setTimeout(() => {
-                            listeConnexions[indexConn - 1].send("^^////" + String(listePaires));
-                        }, 2000)
-                        listeConnexions[indexConn].send(listePaires);
+                        //setTimeout(() => {
+                        //    listeConnexions[indexConn - 1].send("^^////" + String(listePaires));
+                        //}, 2000)
+                        listeConnexions[indexConn].send("^^////" + String(listePaires));
                         indexConn += 1;
                         break;
                     default:
@@ -130,26 +133,30 @@ function Connexion(e) {
 function paramConn(e) {
     e.on("data", (function (e) {
         if (!String(e).includes("^^////")) {
-            //faire module pour bloquer les messages doubles (genre un tag 'temps' à la ms et on check);
-            console.log("message normal : " + String(e))
             divSms = document.querySelector("#smsContainer");
             divSms.insertAdjacentHTML("beforeend", "<div style='text-align: left;' class='smsTxt'>" + String(e) + "</div>");
             divSms.scrollTop = divSms.scrollHeight;
         } else {
-            console.log("message spé : " + String(e))
             let msg = String(e.replace("^^////", "")).split(',').filter(e => e !== idMoi);
             msg = msg.filter(e => listePaires.indexOf(e) < 0)
-            console.log(msg);
-            if (msg.length != 0) { //pour filtrer le cas appel 1v1
+            if (msg.length != 0) {
                 for (idpeer in msg) {
-                    console.log("connexion à " + msg[idpeer])
                     Connexion(msg[idpeer])
                 };
             };
         };
     }));
-    e.on("close", (function (e) {
-        swal(e.peer + ' s\'est deconnecté.')
+    e.on("close", (function () {
+        for (conn in listeConnexions) {
+            if (listeConnexions[conn]._open === false) {
+                try {
+                    document.querySelector("#a" + listeConnexions[conn].peer).parentElement.remove();
+                    listeConnexions[conn] = 'closed';
+                    listePaires.splice(listePaires.indexOf(listeConnexions[conn].peer), 1)
+                } catch (e) {}
+            }
+        }
+
     }));
     lastPressed = "none";
     //on check si on est sur le menu de conv ou pas, si c'est pas le cas on y va
@@ -208,7 +215,7 @@ function CallDude(e) {
 
 var dicoZones = {
     returnArrow: '<img alt="Logo de Ecoute.app" class="nudeLogo" src="assets/ecoute.svg" style="height: 150px; filter: brightness(1.1);"><input class="inputEcoute" id="inputChanmax" placeholder="Ton nom...">',
-    BtnAleatoire: '<img src="assets/ecoute.svg" style="height: 100px; filter: brightness(1.1); opacity:.5">Mode productif en construction.',
+    BtnAleatoire: '<img src="assets/ecoute.svg" style="height: 100px; filter: brightness(1.1); opacity:.5">Salles d\'écoute en construction.',
     BtnParam: "<div style='padding: 10px; max-width:550px;'><h5>Ecoute,</h5><p>Dès l'instant où la connexion est établie entre vous, plus rien n'existe en dehors de votre conversation. <br>Pas de serveurs, publicités, trackers... Rien.<br>Lorsque tout disparaît, il ne reste plus que vous, votre parole et votre <strong>écoute.</strong></p><p>Profitez, personne ne vous regarde.</p><p>-B</p></div>",
     BtnConnaissance: '<h4>Toi :</h4><div id="monIdFrr" onclick="copyToClipboard();swal(\'Ton lien a bien été copié.\')"></div><div id="qrcode"></div><hr><h4>Lui/Elle :</h4><span style="width:60%"><input class="inputEcoute col" type="text" placeholder="Son nom unique..." id="IdDuContact"></span><button id="btn-connex" onclick="Connexion(document.getElementById(\'IdDuContact\').value)" disabled>Connexion</button>',
     BtnUIMessages: '<div style="display: flex; flex-flow: column; height: 100%; width:95%;"><h4 id="titreConv">_messages</h4><div class="convVidContainer"></div><div class="myVidContainer"><div class="vidbloc"><video data-etatcarre="min" id="it-sm-ee" onclick="vidFullScreen(this)" muted></video></div></div><div class="row text-center"><div class="col-md-4 col-s-12"><button id="callBtn" class="buttonEct" onclick="CallDude(\'video\')">📷 Appel vidéo</button></div><div class="col-md-4 col-s-12 d-none d-md-block"><button id="callBtn" class="buttonEct" onclick="CallDude(\'ecran\')">💻 Partage d\'écran</button></div><div class="col-md-4 col-s-12"><button id="callBtn" class="buttonEct" onclick="callDude(\'audio\')">📞 Appel vocal</button></div></div><div class="txtDiv" id="smsContainer"></div><span class="txtDiv"><input type="text" class="col-10 inputEcoute" style="background-color: #efefefbe;" placeholder="Message..." id="idmsgAEnvoyer"><button class="col-2 buttonEct" onclick="SendMessage();" style="background-color: transparent;"><img src="assets/send.svg"></button></span></div>'
@@ -289,6 +296,7 @@ function closeBackToMenu(e) {
                 peer.destroy();
                 changementDeMenu(e);
                 idMoi = '';
+                document.querySelector("#connexStat").innerHTML = "<span id='connexStat'>Connexion...</span>"
                 break;
             default:
                 break
